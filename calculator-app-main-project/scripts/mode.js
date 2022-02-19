9ee57b9cd65e12be3265119e132977ef8f75c464
blarrklad <89408854+blarrklad@users.noreply.github.com>
2022-02-19 02:37:19 +0100

    Add files via upload

---
+++ calculator-app-main-project/scripts/mode.js
@@ -0,0 +1,32 @@
+let mode = localStorage.getItem('calc-mode');
+let button = document.querySelector('[data-toggle]')
+
+function pickTheme() {
+  if (mode == 'dark') {
+    paleTheme();
+    mode = localStorage.getItem('calc-mode');
+  } else {
+    darkTheme();
+    mode = localStorage.getItem('calc-mode');
+  }
+}
+
+function darkTheme() {
+  button.src = 'images/icon_moon.svg';
+  document.body.classList.add('dark');
+  localStorage.setItem('calc-mode', 'dark');
+}
+
+function paleTheme() {
+  button.src = 'images/icon_sun.svg';
+  document.body.classList.remove('dark');
+  localStorage.setItem('calc-mode', null);
+}
+
+window
+.addEventListener('DOMContentLoaded', () => {
+  mode == 'dark' ? darkTheme() : paleTheme();
+});
+
+button
+.addEventListener('click', () => pickTheme());
